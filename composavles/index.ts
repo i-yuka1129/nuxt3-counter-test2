c5c6e07209533b346cdbb571c4bac9b9d6304e37
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-11-20 12:54:48 +0900

    inc,decの設定

--- composavles/index.ts
+++ composavles/index.ts
@@ -1,5 +1,7 @@
 import type { Ref } from 'vue'
 export const inc = (counter: Ref<number>) => () => counter.value++
-export const dec = (counter: Ref<number>) => () => if() {
-  counter.value++
+export const dec = (counter: Ref<number>) => () => {
+  if(counter.value > 0) {
+  counter.value--
+  }
 }
